73d45944da06bd63df681296dd447b5e951d457e
Wei Lyu <lvwei1001@live.com>
2019-01-20 01:22:39 +0900

    change dist folder

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/angular-tour-of-heroes'));
 
 app.listen(process.env.PORT || 8080);
 
 // PathLocationStrategy
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/angular-tour-of-heroes/index.html'));
 })
 
 console.log('Console listening!');
