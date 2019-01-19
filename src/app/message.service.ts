6eb7e6b6b0343221b8ad524238d7a87cd8f3cc96
Wei Lyu <lvwei1001@live.com>
2019-01-19 21:34:25 +0900

    add message

---
+++ src/app/message.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { MessagesComponent } from './messages/messages.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  messages: string[] = [];
+
+  constructor() { }
+
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
+
+}
