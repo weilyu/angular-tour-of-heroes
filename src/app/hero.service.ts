7f411dac1941a20abd52b0f44891395e1527fd0c
Wei Lyu <lvwei1001@live.com>
2019-01-20 12:32:14 +0900

    edit hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 
 
@@ -12,6 +12,10 @@ import { catchError, map, tap } from 'rxjs/operators';
 export class HeroService {
 
   private heroUrl = 'api/heroes';
+
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(
     private http: HttpClient,
@@ -30,6 +34,14 @@ export class HeroService {
       tap(_ => this.log('fetched hero id=' + id)),
       catchError(this.handleError<Hero>('getHero id=' + id))
     );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroUrl, hero, this.httpOptions)
+      .pipe(
+        tap(_ => this.log(`update hero id=${hero.id}`)),
+        catchError(this.handleError<any>('updateHero'))
+      );
   }
 
   private log(message: string) {
