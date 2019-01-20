c590e533f56522e82c7dd254502b1175fd6b16ab
Wei Lyu <lvwei1001@live.com>
2019-01-20 11:12:04 +0900

    move hero detail out of hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -25,12 +25,4 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
-  onSelect(hero: Hero): void {
-    if (this.selectedHero === hero) {
-      this.selectedHero = null;
-    } else {
-      this.selectedHero = hero;
-    }
-  }
-
 }
